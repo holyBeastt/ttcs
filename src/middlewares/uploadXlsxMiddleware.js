18b39678733dc0252ac9eda6e579d5cccd8925d1
holyBeast <doanbanana2003@gmail.com>
2024-11-16 21:38:08 +0700

    test

--- src/middlewares/uploadXlsxMiddleware.js
+++ src/middlewares/uploadXlsxMiddleware.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 500 * 1024 * 1024 }, // Giới hạn dung lượng 500MB
+  limits: { fileSize: 5000000000000000 * 1024 * 1024 }, // Giới hạn dung lượng 500MB
 });
 
 module.exports = upload;
