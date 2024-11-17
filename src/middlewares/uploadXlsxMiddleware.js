d2a2e6d3cb85069dee788e6a68c3e2b9955da4ad
holyBeast <doanbanana2003@gmail.com>
2024-11-17 08:10:39 +0700

    tăng lại limit

--- src/middlewares/uploadXlsxMiddleware.js
+++ src/middlewares/uploadXlsxMiddleware.js
@@ -15,8 +15,7 @@ const upload = multer({
   storage,
   //limits: { fileSize: 5000000000000000 * 1024 * 1024 }, // Giới hạn dung lượng 500000000000000MB
   //limits: { fileSize: 9000000000 * 1024 * 1024 }, // Giới hạn dung lượng 9 PB
-  // limits: { fileSize: Number.MAX_SAFE_INTEGER }, // ~9 petabytes
-  limits: { fileSize: 5 * 1024 * 1024 }, // Giới hạn dung lượng 9 PB
+  limits: { fileSize: Number.MAX_SAFE_INTEGER }, // ~9 petabytes
 });
 
 module.exports = upload;
