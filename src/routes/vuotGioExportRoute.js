56e2dea349ac6633bc1c23c58cba2c7bcdf38441
holyBeast <doanbanana2003@gmail.com>
2024-12-21 16:17:23 +0700

    chỉ suggest các giảng viên trong khoa

--- src/routes/vuotGioExportRoute.js
+++ src/routes/vuotGioExportRoute.js
@@ -1,28 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { exportVuotGio, getGiangVienList } = require('../controllers/vuotGioExportController');
+const {
+  exportVuotGio,
+  getGiangVienList,
+} = require("../controllers/vuotGioExportController");
 
 // Định nghĩa các route
-router.get('/vuotGioExport', (req, res) => {
-    res.render('vuotGioExport');
+router.get("/vuotGioExport", (req, res) => {
+  res.render("vuotGioExport");
 });
-router.get('/api/giangvien', getGiangVienList);
-router.get('/api/export-vuot-gio', exportVuotGio);
+router.get("/api/giangvien", getGiangVienList);
+router.get("/api/export-vuot-gio", exportVuotGio);
 
 // Định nghĩa route cho API giảng viên
-router.get('/api/giangvien', async (req, res) => {
-    let connection;
-    try {
-        connection = await createPoolConnection();
-        const query = 'SELECT GiangVien, Khoa FROM giangday';
-        const [results] = await connection.query(query);
-        res.json(results);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        res.status(500).send("Internal Server Error");
-    } finally {
-        if (connection) connection.release();
-    }
-});
+// router.get('/api/giangvien', async (req, res) => {
+//     let connection;
+//     try {
+//         connection = await createPoolConnection();
+//         const query = 'SELECT GiangVien, Khoa FROM giangday';
+//         const [results] = await connection.query(query);
+//         res.json(results);
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//         res.status(500).send("Internal Server Error");
+//     } finally {
+//         if (connection) connection.release();
+//     }
+// });
 
 module.exports = router; // Xuất router
