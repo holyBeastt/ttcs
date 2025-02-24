27d427d575b72b05fc80bba5258093edbd39b4df
LinhHoangLuu <luuhoanglinh.2003@gmail.com>
2025-02-24 23:04:06 +0700

    thông báo ; convert chuẩn đầu vào của heidisql

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -301,16 +301,13 @@ const addMessage = async (req, res) => {
   }
 };
 
-function convertDateFormat(dateStr) {
-  const date = new Date(dateStr);
-  const mm = String(date.getMonth() + 1).padStart(2, '0'); // Không dùng UTC
-  const dd = String(date.getDate()).padStart(2, '0');      // Không dùng UTC
-  const yyyy = date.getFullYear();                         // Không dùng UTC
-  const hh = String(date.getHours()).padStart(2, '0');     // Giờ (Không dùng UTC)
-  const min = String(date.getMinutes()).padStart(2, '0');  // Phút (Không dùng UTC)
-
-  // console.log(mm, dd, yyyy, hh, min);
-  return `${yyyy}-${mm}-${dd} ${hh}:${min}`;
+function convertToMySQLFormat(dateStr) {
+  // Thay 'T' bằng khoảng trắng
+  let formattedDate = dateStr.replace('T', ' ');
+  // Thêm ':00' vào cuối để biểu diễn giây
+  formattedDate += ':00';
+  console.log(formattedDate);
+  return formattedDate;
 }
 
 const updateMessage = async (req, res) => {
@@ -329,7 +326,7 @@ const updateMessage = async (req, res) => {
       const query = `UPDATE thongbao SET Title = ?, LoiNhan = ?, Deadline = ?, HetHan = ? WHERE id = ?`;
 
       // Thực hiện câu truy vấn
-      await connection.query(query, [tieuDe, loiNhan, convertDateFormat(deadline), isChecked, id]);
+      await connection.query(query, [tieuDe, loiNhan, convertToMySQLFormat(deadline), isChecked, id]);
     }
 
     // Redirect về trang thay đổi thông báo
