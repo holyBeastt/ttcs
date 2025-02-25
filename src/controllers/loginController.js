81926f4e2484a4c5e7cf215b5102bcf9137d9509
tuananh <tuananh0410.03@gmail.com>
2025-02-25 16:18:44 +0700

    log đồ án

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -78,7 +78,7 @@ const createPoolConnection = require("../config/databasePool");
 const { use } = require("../routes/adminRoute");
 require("dotenv").config();
 
-const createTrigger = async (connection, userId, tenNhanVien) => {
+const createTriggermoigiang = async (connection, userId, tenNhanVien) => {
   // Tạo câu lệnh SQL để tạo trigger
   const dropTriggerQuery = `DROP TRIGGER IF EXISTS log_changes;`;
   const triggerQuery = `
@@ -125,7 +125,7 @@ BEGIN
   IF OLD.GiaoVien != NEW.GiaoVien THEN
       SET change_message = CONCAT(change_message, 'Giảng viên cho môn "',  NEW.LopHocPhan, ' - ', NEW.TenLop, '": từ "', OLD.GiaoVien, '" thành "', NEW.GiaoVien, '". ');
   END IF;
-  
+
   -- Nếu có thay đổi, ghi lại thông tin vào bảng lichsunhaplieu
   IF change_message != '' THEN
       INSERT INTO lichsunhaplieu (id_User, TenNhanVien, LoaiThongTin, NoiDungThayDoi, ThoiGianThayDoi)
@@ -155,6 +155,82 @@ END;
   }
 };
 
+const createTriggerdoan = async (connection, userId, tenNhanVien) => {
+  // Tạo câu lệnh SQL để tạo trigger
+  const dropTriggerQuery = `DROP TRIGGER IF EXISTS doantriggers;`;
+  const triggerQuery = `
+  CREATE TRIGGER doantriggers
+  AFTER UPDATE ON doantotnghiep
+  FOR EACH ROW
+BEGIN
+  DECLARE change_message VARCHAR(255) DEFAULT '';
+  DECLARE loai_thong_tin VARCHAR(50) DEFAULT 'Thay đổi thông tin đồ án';
+
+  -- Kiểm tra cột Giangvien1real
+  IF OLD.GangVien1Real != NEW.GiangVien1Real THEN
+     SET change_message = CONCAT(change_message, 'Giảng Viên 1 cho đồ án "', NEW.TenDeTai,'": từ "', OLD.GiangVien1Real, '" thành "', NEW.GiangVien1Real, '". ');
+  END IF;
+
+   -- Kiểm tra cột Giangvien2real
+  IF OLD.GangVien2Real != NEW.GiangVien2Real THEN
+     SET change_message = CONCAT(change_message, 'Giảng Viên 2 cho đồ án "', NEW.TenDeTai,'": từ "', OLD.GiangVien1Real, '" thành "', NEW.GiangVien1Real, '". ');
+  END IF;
+
+  -- Kiểm tra cột KhoaDuyet
+  IF OLD.KhoaDuyet != NEW.KhoaDuyet THEN
+      IF OLD.KhoaDuyet = 0 AND NEW.KhoaDuyet = 1 THEN
+          SET change_message = CONCAT(change_message, 'Khoa thay đổi duyệt đồ án "',  NEW.TenDeTai,'": Đã duyệt. ');
+      ELSEIF OLD.KhoaDuyet = 1 AND NEW.KhoaDuyet = 0 THEN
+          SET change_message = CONCAT(change_message, 'Khoa thay đổi duyệt đồ án "',  NEW.TenDeTai,' ": Hủy duyệt. ');
+      END IF;
+  END IF;
+
+  -- Kiểm tra cột DaoTaoDuyet
+  IF OLD.DaoTaoDuyet != NEW.DaoTaoDuyet THEN
+      IF OLD.DaoTaoDuyet = 0 AND NEW.DaoTaoDuyet = 1 THEN
+          SET change_message = CONCAT(change_message, 'Đào tạo thay đổi duyệt đồ án "',  NEW.TenDeTai,'": Đã duyệt. ');
+      ELSEIF OLD.DaoTaoDuyet = 1 AND NEW.DaoTaoDuyet = 0 THEN
+          SET change_message = CONCAT(change_message, 'Đào tạo thay đổi duyệt đồ án "',  NEW.TenDeTai,'": Hủy duyệt. ');
+      END IF;
+  END IF;
+
+  -- Kiểm tra cột TaiChinhDuyet
+  IF OLD.TaiChinhDuyet != NEW.TaiChinhDuyet THEN
+      IF OLD.TaiChinhDuyet = 0 AND NEW.TaiChinhDuyet = 1 THEN
+          SET change_message = CONCAT(change_message, 'Tài chính thay đổi duyệt đồ án "',  NEW.TenDeTai,'": Đã duyệt. ');
+      ELSEIF OLD.TaiChinhDuyet = 1 AND NEW.TaiChinhDuyet = 0 THEN
+          SET change_message = CONCAT(change_message, 'Tài chính thay đổi duyệt môn "',  NEW.TenDetai,'": Hủy duyệt. ');
+      END IF;
+  END IF;
+  
+  -- Nếu có thay đổi, ghi lại thông tin vào bảng lichsunhaplieu
+  IF change_message != '' THEN
+      INSERT INTO lichsunhaplieu (id_User, TenNhanVien, LoaiThongTin, NoiDungThayDoi, ThoiGianThayDoi)
+      VALUES (
+        ${userId},  
+          '${tenNhanVien}',
+          loai_thong_tin,  -- Loại thông tin
+          change_message,  -- Nội dung mới với thông báo thay đổi
+          NOW()  -- Thời gian thay đổi
+      );
+  END IF;
+END;
+
+  `;
+  try {
+    // Lấy kết nối từ pool
+    const connection = await createPoolConnection();
+    try {
+      // Tạo trigger sau khi đăng nhập thành công
+      await connection.query(dropTriggerQuery);
+      await connection.query(triggerQuery);
+    } finally {
+      if (connection) connection.release(); // Giải phóng kết nối
+    }
+  } catch (error) {
+    console.error("Lỗi khi tạo trigger:", error.message);
+  }
+};
 const login = async (req, res) => {
   const { username, password } = req.body;
   let connection;
