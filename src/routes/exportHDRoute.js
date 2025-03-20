0dcd3e96642d1764a86ce7ce77b8d39c4a53a5b9
holyBeast <doanbanana2003@gmail.com>
2025-03-20 17:01:58 +0700

    xuất thông tin bổ sung gvm

--- src/routes/exportHDRoute.js
+++ src/routes/exportHDRoute.js
@@ -8,5 +8,16 @@ router.get("/exportHD", exportHDController.getExportHDSite);
 // Route để xuất hợp đồng cho nhiều giảng viên
 router.get("/exportHD/downloadAll", exportHDController.exportMultipleContracts);
 
+// Phần xuất thông tin bổ sung giảng viên mời
+
+router.get(
+  "/api/hd-gvm/additional-file-site",
+  exportHDController.getExportAdditionalInfoGvmSite
+);
+
+router.get(
+  "/api/hd-gvm/export-additional-file",
+  exportHDController.exportAdditionalInfoGvm
+);
 
 module.exports = router;
