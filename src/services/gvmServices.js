5c3aade798657cc9a8161ec11c5606b94fc4c5c1
holyBeast <doanbanana2003@gmail.com>
2025-12-14 21:22:07 +0700

    feat thêm api lấy danh sách hệ đào tạo

--- src/services/gvmServices.js
+++ src/services/gvmServices.js
@@ -1,34 +1,5 @@
-// const express = require("express");
-// const multer = require("multer");
-// const connection = require("../config/database");
-
-// const router = express.Router();
-// const createConnection = require("../config/databaseAsync");
-
-// const getGvmLists = async (req, res) => {
-//   try {
-//     const connection2 = await createConnection();
-//     const query = "SELECT * FROM `gvmoi`";
-//     const [results] = await connection2.query(query);
-
-//     // Gửi danh sách giảng viên dưới dạng mảng
-//     return results; // Chỉ gửi kết quả mảng
-//   } catch (error) {
-//     console.error("Error fetching GVM lists: ", error);
-//     return res.status(500).send("Internal server error"); // Trả về chuỗi thông báo lỗi
-//   }
-// };
-
-// // Xuất các hàm để sử dụng trong router
-// module.exports = {
-//   getGvmLists,
-// };
-
-const express = require("express");
-const multer = require("multer");
-
-const router = express.Router();
 const createPoolConnection = require("../config/databasePool");
+const pool = require("../config/Pool");
 
 const getGvmLists = async (req, res) => {
   let connection;
@@ -47,9 +18,19 @@ const getGvmLists = async (req, res) => {
   }
 };
 
+const getHeDaoTaoLists = async (req, res) => {
+  try {
+    const query = "SELECT id, he_dao_tao FROM `he_dao_tao`";
+    const [results] = await pool.query(query);
+    return results;
+  } catch (error) {
+    console.error("Error fetching He Dao Tao lists: ", error);
+    return res.status(500).send("Internal server error");
+  }
+}
+
 // Xuất các hàm để sử dụng trong router
 module.exports = {
   getGvmLists,
+  getHeDaoTaoLists,
 };
-
-// Không được bỏ
